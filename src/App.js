fdbf83ff94ce50e44f8efdf1cd131b2e49390b33
Nyx-95 <85751219+Nyx-95@users.noreply.github.com>
2022-10-14 16:36:38 +0500

    lil changes

--- src/App.js
+++ src/App.js
@@ -2,30 +2,26 @@ import './App.css';
 import Header from './Components/header/Header';
 import Login from "./Components/Login/Login";
 import Home from "./Home"
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
 
 function App() {
 
-  const [username,setUsername] = useState('');
+  const [username, setUsername] = useState('');
   const getName = (hi) => {
     setUsername(hi);
   }
 
-
-
   return (
-    
     <div className="App">
       <BrowserRouter>
-      <Header name={username}></Header>
+        <Header name={username}></Header>
         <Routes>
-          <Route path="/home" element={<Home msgname={username}/>} />
-          <Route path="/" element={<Login getName={getName}/>} />
+          <Route path="/home" element={<Home msgname={username} />} />
+          <Route path="/" element={<Login getName={getName} />} />
         </Routes>
       </BrowserRouter>
     </div>
-
   );
 }
 
