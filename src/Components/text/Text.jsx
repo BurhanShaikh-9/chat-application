4e0b7fcb7d347bd3f44cc13f8a76bf09ff33b44c
Nyx-95 <85751219+Nyx-95@users.noreply.github.com>
2022-10-16 08:48:32 +0500

    update

--- src/Components/text/Text.jsx
+++ src/Components/text/Text.jsx
@@ -13,20 +13,20 @@ function Text({getMsg}) {
     <section className='footer'>
       <div className='container'>
         <div className='footerdiv'>
-          <form className="row g-3">
-            <div className="col-xl-1 col-sm-1">
+          <div className="row">
+            <div className="col-sm-1 col-md-1">
             <button type="submit" className="btn btn-primary mb-3" id='btun1'>ESC</button>
             </div>
-            <div className="col-xl-9 col-sm-9">
+            <div className="col-sm-9 col-sm-9">
               <input type="text" className="form-control" id="msgId" placeholder="Write Message" ref={inputRef}/>
             </div>
-            <div className="col-xl-1 col-sm-1">
+            <div className="col-sm-1">
               <button type="submit" className="btn btn-primary mb-3" id='btun2'>+</button>
             </div>
-            <div className="col-xl-1 col-sm-1">
+            <div className="col-sm-1">
               <button type="submit" className="btn btn-primary mb-3" onClick={navigateToHome} id='btun3'>Send</button>
             </div>
-          </form>
+          </div>
         </div>
       </div>
     </section>
