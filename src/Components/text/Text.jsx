6922612ac63010fbee2ff082a515b8bba65eaa80
Nyx-95 <85751219+Nyx-95@users.noreply.github.com>
2022-10-16 05:59:02 +0500

    update

--- src/Components/text/Text.jsx
+++ src/Components/text/Text.jsx
@@ -13,19 +13,19 @@ function Text({getMsg}) {
     <section className='footer'>
       <div className='container'>
         <div className='footerdiv'>
-          <form class="row g-3">
-            <div class="col-xl-1 col-sm-1">
-            <button type="submit" class="btn btn-primary mb-3">ESC</button>
+          <form className="row g-3">
+            <div className="col-xl-1 col-sm-1">
+            <button type="submit" className="btn btn-primary mb-3">ESC</button>
             </div>
-            <div class="col-xl-9 col-sm-9">
+            <div className="col-xl-9 col-sm-9">
 
-              <input type="text" class="form-control" id="msgId" placeholder="Write Message" ref={inputRef}/>
+              <input type="text" className="form-control" id="msgId" placeholder="Write Message" ref={inputRef}/>
             </div>
-            <div class="col-xl-1 col-sm-1">
-              <button type="submit" class="btn btn-primary mb-3">+</button>
+            <div className="col-xl-1 col-sm-1">
+              <button type="submit" className="btn btn-primary mb-3">+</button>
             </div>
-            <div class="col-xl-1 col-sm-1">
-              <button type="submit" class="btn btn-primary mb-3" onClick={navigateToHome}>Send</button>
+            <div className="col-xl-1 col-sm-1">
+              <button type="submit" className="btn btn-primary mb-3" onClick={navigateToHome}>Send</button>
             </div>
           </form>
 
