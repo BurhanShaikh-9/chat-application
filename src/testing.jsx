a4a186158f8cb61581131c321af0fa3d2e670c7e
Nyx-95 <85751219+Nyx-95@users.noreply.github.com>
2022-10-18 08:54:57 +0500

    update

--- src/testing.jsx
+++ src/testing.jsx
@@ -81,7 +81,7 @@ export default function App() {
   );
 }
 */
-const baseURL = "https://my-json-server.typicode.com/nyx-95/chat-application/db";
+const baseURL = "https://my-json-server.typicode.com/nyx-95/chat-application/Users";
 
 export default function App() {
   const [post, setPost] = React.useState(null);
@@ -89,16 +89,22 @@ export default function App() {
   React.useEffect(() => {
     axios
     .get(baseURL)
-    .then((res) => {setPost(res.data);
-      console.log(res.data)});
+    .then((res) => {setPost(res.data[0]);
+      console.log(res.data[0])});
   }, []);
-
-  if (!post) return "No post!"
+  function update(){
+    axios
+    .post(baseURL,{name: "Post 4"})
+    .then((res) => {
+      setPost(res.data)
+      console.log(res.data)
+    })
+  }
 
   return (
     <div>
-      <h1>{post.title}</h1>
-     
+      <h1>{post.name}</h1>
+     <button onClick={update}>click</button>
     </div>
   );
 }
