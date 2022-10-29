0f2757d9eea39d6f75e5fc9bc49c785dd6968ed0
Nyx-95 <85751219+Nyx-95@users.noreply.github.com>
2022-10-29 15:05:33 +0500

    update

---
+++ Backend/server.js
@@ -0,0 +1,14 @@
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {})
+
+io.on("connection", (socket) =>{
+    console.log("what is socket", socket)
+    console.log("socket is activated")
+    socket.on("chat", (payload) => {
+        console.log("what is payload", payload);
+        io.emit("chat", payload);
+    });
+});
+
+app.listen(3000,() => console.log("server is active..."))
