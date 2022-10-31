a697d9896c2425546e97495b978d7cf5f7681a2f
Nyx-95 <shaikhshady6@gmail.com>
2022-10-31 01:11:40 -0700

    update

--- fontend/src/App.js
+++ fontend/src/App.js
@@ -3,7 +3,10 @@ import Header from './Components/Header/Header';
 import Login from "./Components/Login/Login";
 import Body from "./Components/Body/Body";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect} from 'react';
+import io from 'socket.io-client';
+const socket = io.connect("http://localhost:3001");
+
 
 function App() {
   /*get username*/
@@ -11,18 +14,31 @@ function App() {
   const getName = (name) => {
     setUsername(name);
   }
+
+
+  
   /*get text message */
-  const [textmessage, setTextmessage] = useState('');
+  const [message, setMessage] = useState("");
   const gettingText = (msgs) => {
-    setTextmessage(msgs);
+    setMessage(msgs);
   }
+
+  socket.emit("send_message", {message});
+
+  const [messageReceived, setMessageReceived] = useState("");
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessageReceived(data.message);
+    });
+  }, [socket]);
 
   return (
     <div className="App">
       <BrowserRouter>
         <Header name={username}></Header>
         <Routes>
-          <Route path="/home" element={<Body msgname={username} getMessage={gettingText} msgText={textmessage}/>}  />
+          <Route path="/home" element={<Body msgname={username} getMessage={gettingText} msgText={message} msgrecieved={messageReceived}/>}  />
           <Route path="/" element={<Login getNames={getName} />} />
         </Routes>
       </BrowserRouter>
